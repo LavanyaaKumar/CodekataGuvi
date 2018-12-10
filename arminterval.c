696f5692835a41e61eb8f850a5f8a0eab2d5d40a
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-10 12:24:41 +0530

    Create arminterval.c

---
+++ arminterval.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,i,j,r,temp;
+	scanf("%d%d",&x,&y);
+	for(i=x+1;i<y;i++)
+	{
+		temp=i;
+		int arm=0;
+		while(temp!=0)
+		{
+			r=temp%10;
+			arm=(r*r*r)+arm;
+			temp=temp/10;
+		}
+		if(i==arm)
+		{
+			printf("%d ",i);
+		}
+	}
+  return 0;
+}
