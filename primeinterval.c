9b6b0502543a6bd55c10c2f798a65e93e2e32833
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-10 11:49:56 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,i,j;
+	scanf("%d%d",&x,&y);
+	for(i=x+1;i<y;i++)
+	{
+		int prime=0;
+		for(j=2;j<i;j++)
+		{
+			if(i%j==0)
+			{
+				prime=1;
+				break;
+			}
+	}
+		if(prime==0)
+		printf("%d ",i);
+	}
+	return 0;
+}
