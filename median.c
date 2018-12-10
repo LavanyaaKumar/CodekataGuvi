b004a1c94a35f7a3d134919ffd57b375a3be56bb
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-10 15:42:15 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[10],i,j,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	
+	if(n%2!=0)
+	{
+		int m=n/2;
+		printf("%d ",a[m]);
+	}
+	else
+	{
+		int d=(a[n/2]+a[n/2]-1)/2;
+		printf("%d ",d);
+	}
+}
