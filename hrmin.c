70b046715a1c4ec96c9d440cfe9dcfebfbb502a7
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-12 12:09:00 +0530

    Create hrmin.c

---
+++ hrmin.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int t,h,m;
+	scanf("%d",&t);
+	if(t<60)
+	{
+		printf("0\t%d",t);
+		
+	}
+	else
+	{
+		h=t/60;
+		m=t%60;
+		printf("%d\t%d",h,m);
+	}
+	return 0;
+}
