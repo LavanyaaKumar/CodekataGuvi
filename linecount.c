20bcdd492872706f4a6e6b5ad6e01897768bfbe3
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-13 11:57:13 +0530

    Create linecount.c

---
+++ linecount.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[20];
+	gets(a);
+	int i,n,count=0;
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]=='.')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+}
