af07d8678609c088a9fb35084d78b0d8895d05ea
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-13 12:40:01 +0530

    Update countspchar.c

--- countspchar.c
+++ countspchar.c
@@ -8,7 +8,7 @@ int main(void)
 	n=strlen(a);
 	for(i=0;i<n;i++)
 	{
-		if((a[i]>='a' && a[i]<='z')||(a[i]>='A' && a[i]<='Z')||(a[i]>+'0' && a[i]<='9')||(a[i]==' '))
+		if((a[i]>='a' && a[i]<='z')||(a[i]>='A' && a[i]<='Z')||(a[i]>='0' && a[i]<='9'))
 		{
 			continue;
 		}
