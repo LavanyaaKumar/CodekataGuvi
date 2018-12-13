0bccfcf0e17eae0bfd845c3a1fa650c43aa00266
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-13 17:08:03 +0530

    Create strntimes.c

---
+++ strntimes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+     int n,i;
+     char a[10];
+     scanf("%s",a);
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+     	printf("%s\n",a);
+     }
+     return 0;
+}
