3b3af99ba21d71c14d65867310bf671d21d17d8c
lavanyaa3216 <45381239+lavanyaa3216@users.noreply.github.com>
2018-12-13 13:38:39 +0530

    Create nfibo.c

---
+++ nfibo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n,i;
+	scanf("%d",&n);
+	int a=0,b=1,c;
+	printf("%d ",b);
+    for(i=0;i<n;i++)
+    {
+    	c=a+b;
+    	printf("%d ",c);
+    	a=b;
+    	b=c;
+    }
+}
