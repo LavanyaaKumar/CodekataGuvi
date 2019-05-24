a6b62784eb7b2b3a42f14887c75c6ad70d8e82fa
Lavanya <45381239+lavanyaa3216@users.noreply.github.com>
2019-05-24 15:16:19 +0530

    Update noinarray.c

--- noinarray.c
+++ noinarray.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
- int a[10],n,k,i,y=0;
- scanf("%d%d",&n,&k);
- for(i=0;i<n;i++)
- {
- 	scanf("%d",&a[i]);
- }
- for(i=0;i<n;i++)
- {
- 	if(k==a[i])
-    y=1;
-	 break;
- }
+int n1,n2,k,a[10],i,y=0;
+scanf("%d%d%d",&n1,&n2,&k);
+for(i=0;i<k;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+	if((n1||n2)==a[i])
+	y=1;
+	break;
+}
 if(y==1)
-	printf("yes");
+printf("yes");
 else
-	printf("no");
+printf("no");
 }
