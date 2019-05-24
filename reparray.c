6774b77aeb6c251264adaa1fcc2f1622b59d35cd
Lavanya <45381239+lavanyaa3216@users.noreply.github.com>
2019-05-24 15:52:03 +0530

    Create reparray.c

---
+++ reparray.c
@@ -0,0 +1,15 @@
+int main(void) {
+	int n,k,a[10],i,count=0;
+	scanf("%d%d",&n,&k);
+	for(i=0;i<n;i++)
+    {
+    	scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+      
+    	if(a[i]==k)
+    	count++;
+    }
+    printf("%d",count);
+}
